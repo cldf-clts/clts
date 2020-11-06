59a62e41cba44c75fe9a9f36d086e080b9dfcef2
Johann-Mattis List <LinguList@users.noreply.github.com>
2020-11-06 07:24:05 +0100

    Create parse.py

---
+++ sources/saphon/parse.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+import re
+
+sounds = []
+req = urlopen('https://linguistics.berkeley.edu/~saphon/en/phonemes.php')
+text = req.read().decode('utf-8')
+for idx, sound in re.findall('<span[^>]*f=([0-9]*)[^>]*>([^<]*)</span>', text, re.DOTALL):
+    sounds += [[idx, sound]]
+with open('graphemes.tsv', 'w') as f:
+    f.write('BIPA\tGRAPHEME\tID\n')
+    for a, b in sounds:
+        f.write('\t'+a+'\t'+b+'\n')
+print('found {0} sounds'.format(len(sounds)))
