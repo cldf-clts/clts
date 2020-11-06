0c499e8d4d2abc433e0fdf84706ab740af7cbb7f
Johann-Mattis List <LinguList@users.noreply.github.com>
2020-11-06 07:37:25 +0100

    Create parse.py

---
+++ sources/sala/parse.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+import re
+
+sounds = []
+req = urlopen('http://sumale.vjf.cnrs.fr/phono/Init00.php')
+text = req.read().decode('utf-8')
+for idx, sound in re.findall("<A href=.*?ChoixCellule=([A-Z0-9]*)[^A-Z0-9][^>]*>([^>]*)</A>",
+        text, re.DOTALL):
+    sounds += [[idx, sound]]
+with open('graphemes.tsv', 'w') as f:
+    f.write('BIPA\tGRAPHEME\tID\n')
+    for a, b in sounds:
+        f.write('\t'+b+'\t'+a+'\n')
+print('found {0} sounds'.format(len(sounds)))
